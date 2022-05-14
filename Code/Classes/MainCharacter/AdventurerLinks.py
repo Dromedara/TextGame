2cc5e27c048e3081d6144054e32dfedb2c59aeab
Dromedara <pavlishina_ira@mail.ru>
2022-05-14 10:53:15 +0300

    'aaa'

--- Code/Classes/MainCharacter/AdventurerLinks.py
+++
@@ -1,7 +0,0 @@
-import Code.Classes.MainCharacter.BasicSkills
-
-active_adventurer_dict = {
-    'simple_punch': Code.Classes.MainCharacter.BasicSkills.AdventurerFuncs.simple_punch
-}
-
-passive_adventurer_dict = {}
--- Code/Locations/Warehouse/EquipSubFuncs.py
+++
@@ -1,7 +0,0 @@
-class Checker:
-
-    @staticmethod
-    def add_potion(slots, key):
-        if slots.get(key) is None:
-            slots[key] = []
-        return slots
